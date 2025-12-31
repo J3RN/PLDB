83038e7902a0a1d031083ae94b563775c1c4edfb
Jonathan Arnett <j3rn@j3rn.com>
2025-12-31 10:29:40 -0500

    Add flake.nix

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "Programming Language Database (PLDB)";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+          {
+            devShells.default = pkgs.mkShell {
+              packages = with pkgs; [
+                dhall
+                dhall-json
+                dhall-lsp-server
+              ];
+            };
+          }
+    );
+}
